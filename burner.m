28fdb565a00c979dab19deb012d39c500940130b
Niraj Suresh <nsuresh9@gatech.edu>
2016-07-18 19:55:34 +0100

    Change To4 (burner) to input

--- burner.m
+++ burner.m
@@ -1,10 +1,8 @@
 %ramjet doesn't have a burner
 %turbojet & turbofan
-function [To4, Po4, f] = burner(yb, Po3, To3, Nb, hr, R, Prb)
+function [Po4, f] = burner(yb, Po3, To3, To4, Nb, hr, R, Prb)
 
     cpb = R * (yb / yb-1);
-
-    To4 = (To3 + ((Nb*f*hr) / cpb)) * (1 / (1+f));
     
     f = ((To4/To3) - 1) / ((Nb*hr / cpb*To3) - (To4/To3));
 
