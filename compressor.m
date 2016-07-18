f9477aadfeeeac24d2ce66c65e2eadb735927a26
saradydx <saradydx@gmail.com>
2016-07-18 20:17:31 +0100

    More updated stuff

--- compressor.m
+++ compressor.m
@@ -1,4 +1,4 @@
-function [To3, Po3] = compressor(To2, Po2, yc)
+function [To3, Po3] = compressor(To2, Po2, yc, Prc, NPc)
     % Same for turbojet, turbofan
     Po3 = Po2 * Prc;
     To3 = To2 * (1 + (1 / NPc) * (Prc ^ ((yc - 1) / yc) - 1));
