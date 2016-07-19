007e5002282f30448455aebbafae7ff7895e75ba
saradydx <saradydx@gmail.com>
2016-07-19 19:09:03 +0100

    Some modifications + mixer structures

--- bypassFan.m
+++ bypassFan.m
@@ -1,6 +1,8 @@
 function [To2, Po2] = bypassFan(Po1, To1, Prf)
    % Only for turbofan
-   % To2, Po2
+
    Po2 = Po1 * Prf;
+   
    To2 = To1 * Prf ^ ((y - 1) / y); 
+   
 end
---
+++ turbineMixer.m
@@ -0,0 +1,5 @@
+function [Tmax] = turbineMixer()
+
+Tmax = Tmaxo + (cb1 * sqrt(b/bmax));
+
+end
