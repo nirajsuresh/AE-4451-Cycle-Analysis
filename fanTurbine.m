c5dcbee2ebe3630dc22197dc21b9512809bdc35b
Niraj Suresh <nsuresh9@gatech.edu>
2016-07-19 22:04:50 +0100

    Fixed fan turbine

--- fanTurbine.m
+++ fanTurbine.m
@@ -1,11 +1,11 @@
-function [To7, Po7, wft] = fanTurbine(To6, R, f, B, yft)
+function [To52, Po52, wft] = fanTurbine(To5m, R, f, B, yft)
 
 cpft = R * (yft / (yft - 1));
 
-To7 = To6 - ((cpft * (To3 - To2) * (1 + B)) / ((cpft) * (1 + f)));
+To52 = To6 - ((cpft * (To3 - To2) * (1 + B)) / ((cpft) * (1 + f)));
 
-Po7 = Po6 * ((1/Nt) * ((To7/To6) - 1))^(yft/(yft - 1));
+Po52 = Po6 * ((1/Nt) * ((To52/To6) - 1))^(yft/(yft - 1));
 
-wft = (R * (yf/(yf - 1))) * (To7 - To6);
+wft = (R * (yf/(yf - 1))) * (To52 - To5m);
 
 end
